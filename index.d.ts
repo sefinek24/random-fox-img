ce2914e603fc8f6655e0a5bcd67809aadbd516bc
Sefinek <contact@sefinek.net>
2023-10-21 03:29:41 +0200

    Fix types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,14 @@
-interface getRandomFox {
-    success: boolean;
-    status: number;
-    info: {
-        category: string;
-        endpoint: string;
-    };
-    message: string;
+declare module 'random-fox-img' {
+    interface FoxImageData {
+        success: boolean;
+        status: number;
+        info: {
+            category: string;
+            endpoint: string;
+        };
+        message: string;
+    }
+
+    function getRandomFox(): Promise<FoxImageData>;
+    export = getRandomFox;
 }
